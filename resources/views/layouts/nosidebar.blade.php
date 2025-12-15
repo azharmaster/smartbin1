e39642dbc4eb745870fd0aded11f729e4f4ed63c
Nur Atirah <naatirah79@gmail.com>
2025-12-15 09:39:49 +0800

    Update nosidebar.blade.php

--- resources/views/layouts/nosidebar.blade.php
+++ resources/views/layouts/nosidebar.blade.php
@@ -68,7 +68,7 @@
         <!-- Footer -->
         <footer class="main-footer">
             <div class="float-right d-none d-sm-inline">Anything you want</div>
-            <strong>Copyright &copy; 2014-2025 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            <strong>Copyright &copy; 2014-2025 PSENSEHUB/SmartBin.</strong>
             All rights reserved.
         </footer>
 
