9b35767b121349c90b8980cb96479c55073de18c
Nur Atirah <naatirah79@gmail.com>
2025-12-16 10:29:42 +0800

    h

--- resources/views/layouts/nosidebaradmin.blade.php
+++ resources/views/layouts/nosidebaradmin.blade.php
@@ -370,14 +370,14 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">@yield('content_title')</h1>
+                            <h4 class="m-0">@yield('content_title')</h4>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item">
+                                {{-- <li class="breadcrumb-item">
                                     <a href="{{ route('dashboard') }}">Home</a>
-                                </li>
-                                <li class="breadcrumb-item active">@yield('content_title')</li>
+                                </li> --}}
+                                {{-- <li class="breadcrumb-item active">@yield('content_title')</li> --}}
                             </ol>
                         </div>
                     </div>
