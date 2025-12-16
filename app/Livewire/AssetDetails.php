daa54bd99ded43f0bff2281d2059bc62ef61535d
Nur Atirah <naatirah79@gmail.com>
2025-12-16 15:58:19 +0800

    asset details page fixed

--- app/Livewire/AssetDetails.php
+++ app/Livewire/AssetDetails.php
@@ -7,26 +7,31 @@ use App\Models\Asset;
 
 class AssetDetails extends Component
 {
-    public Asset $asset;
+    public $asset;
 
-    public function mount(Asset $asset)
-    {
-        // Asset is already loaded by controller
+public function mount($asset)
+{
+    // $asset could be an ID (int/string) or a model
+    if (is_numeric($asset)) {
+        $this->asset = Asset::with(['floor', 'devices.sensors'])->findOrFail($asset);
+    } elseif ($asset instanceof Asset) {
         $this->asset = $asset->load(['floor', 'devices.sensors']);
+    } else {
+        throw new \Exception('Invalid asset provided');
+    }
+}
+
+public function updatePosition($assetId, $x, $y)
+{
+    $asset = Asset::find($assetId);
+    if ($asset) {
+        $asset->x = $x;
+        $asset->y = $y;
+        $asset->save();
     }
 
-    public function updatePosition($assetId, $x, $y)
-    {
-        $asset = Asset::find($assetId);
-
-        if ($asset) {
-            $asset->x = $x;
-            $asset->y = $y;
-            $asset->save();
-        }
-
-        $this->asset->refresh();
-    }
+    $this->asset->refresh();
+}
 
     public function render()
     {
