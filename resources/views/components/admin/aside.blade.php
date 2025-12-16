af136615776b9d1d4846f6e7263253c1d88e1879
Nur Atirah <naatirah79@gmail.com>
2025-12-16 14:25:25 +0800

    sidebar fixed

--- resources/views/components/admin/aside.blade.php
+++ resources/views/components/admin/aside.blade.php
@@ -9,47 +9,59 @@
 
     <!-- Sidebar Menu -->
     <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <ul class="nav nav-pills nav-sidebar flex-column"
+            data-widget="treeview"
+            role="menu"
+            data-accordion="false">
 
             @foreach ($routes as $route)
                 @php
                     $isActive = request()->routeIs($route['route_active']);
-                    $iconColor = $route['color'] ?? '#000';
+                    $iconColor = $route['color'] ?? '#6c757d'; // fallback
                 @endphp
 
+                {{-- SINGLE LINK --}}
                 @if (!$route['is_dropdown'])
                     <li class="nav-item">
                         <a href="{{ route($route['route_name']) }}"
-                           class="nav-link {{ $isActive ? 'active' : '' }}"
-                           style="color: {{ $iconColor }};">
-                            <i class="nav-icon {{ $route['icon'] }}" style="color: {{ $iconColor }};"></i>
+                           class="nav-link {{ $isActive ? 'active' : '' }}">
+
+                            <i class="nav-icon {{ $route['icon'] }}"
+                               style="color: {{ $iconColor }};"></i>
+
                             <p>{{ $route['label'] }}</p>
                         </a>
                     </li>
+
+                {{-- DROPDOWN --}}
                 @else
-                    @php
-                        $menuOpen = $isActive ? 'menu-open' : '';
-                    @endphp
-                    <li class="nav-item {{ $menuOpen }}">
-                        <a href="#" class="nav-link {{ $isActive ? 'active' : '' }}">
-                            <i class="nav-icon {{ $route['icon'] }}" style="color: {{ $iconColor }};"></i>
+                    <li class="nav-item {{ $isActive ? 'menu-open' : '' }}">
+                        <a href="#"
+                           class="nav-link {{ $isActive ? 'active' : '' }}">
+
+                            <i class="nav-icon {{ $route['icon'] }}"
+                               style="color: {{ $iconColor }};"></i>
+
                             <p>
                                 {{ $route['label'] }}
                                 <i class="right fas fa-angle-left"></i>
                             </p>
                         </a>
+
                         <ul class="nav nav-treeview">
                             @foreach ($route['dropdown'] as $dropdownItem)
                                 @php
                                     $subActive = request()->routeIs($dropdownItem['route_active']);
                                     $subColor = $dropdownItem['color'] ?? $iconColor;
                                 @endphp
+
                                 <li class="nav-item">
                                     <a href="{{ route($dropdownItem['route_name']) }}"
-                                       class="nav-link {{ $subActive ? 'active' : '' }}"
-                                       style="color: {{ $subColor }};">
-                                        <i class="{{ $dropdownItem['icon'] ?? 'fas fa-circle' }} nav-icon" 
+                                       class="nav-link {{ $subActive ? 'active' : '' }}">
+
+                                        <i class="nav-icon {{ $dropdownItem['icon'] }}"
                                            style="color: {{ $subColor }};"></i>
+
                                         <p>{{ $dropdownItem['label'] }}</p>
                                     </a>
                                 </li>
