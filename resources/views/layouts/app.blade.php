5c9902c14609b0ba3850ac1f2edd64d059f8c9bb
mukrizzaa <adammukriz2005@gmail.com>
2026-01-20 11:42:52 +0800

    d

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SMARTBIN</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('uploads/images/bin2.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('uploads/images/logo.png') }}">
     
     <!-- Google Font -->
     <link rel="stylesheet"
