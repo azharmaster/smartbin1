0cd10e3e65a26e019896fc8d74de2148d3e5919f
mukrizzaa <adammukriz2005@gmail.com>
2026-01-21 10:44:12 +0800

    Update index.blade.php

--- resources/views/sensors/index.blade.php
+++ resources/views/sensors/index.blade.php
@@ -1,25 +1,30 @@
 @extends($layout)
 @section('content_title', 'Sensor')
 @section('content')
-<div class="card">
-    <div class="card-header">
-        <h4 class="card-title">Sensor Data</h4>
+
+<div class="card card-success card-outline">
+    <div class="card-header d-flex align-items-center">
+        <h5 class="mb-0">Sensor Data</h5>
+
+        <div class="ms-auto">
+            {{-- keep empty for future button --}}
+        </div>
     </div>
+
     <div class="card-body">
+
         @if ($errors->any())
         <div class="alert alert-danger d-flex flex-column">
             @foreach ($errors->all() as $error)
-            <small class="text-white my-2">{{ $error }}</small>
+                <small class="text-white my-2">{{ $error }}</small>
             @endforeach
         </div>
         @endif
 
-        <div class="d-flex justify-content-end mb-2">
-        </div>
         <div class="table-responsive">
-        <table id="table1" class="table table-bordered table-striped dataTable dtr-inline">
-              <thead>
-                 <tr>
+            <table class="table table-bordered table-striped datatable datatable-buttons">
+                <thead>
+                    <tr>
                         <th>#</th>
                         <th>Device ID</th>
                         <th>Battery</th>
@@ -28,22 +33,23 @@
                         <th>Network</th>
                         <th>Action</th>
                     </tr>
-              </thead>
-              <tbody>
-         
+                </thead>
+                <tbody>
                     @foreach ($sensors as $index => $sensor)
                     <tr>
-                        <td>{{  $index + 1  }}</td>
+                        <td>{{ $index + 1 }}</td>
                         <td>{{ $sensor->device_id }}</td>
                         <td>{{ $sensor->battery }}</td>
                         <td>{{ $sensor->capacity }}</td>
                         <td>{{ $sensor->time }}</td>
                         <td>{{ $sensor->network }}</td>
                         <td>
-                            <div class="d-flex align-items-center justify-content-center">
-                               <a href="{{ route('sensors.destroy', $sensor->id) }}" data-confirm-delete="true" class="btn btn-danger btn-sm">
-    <i class="fas fa-trash-alt text-white"></i>
-</a>
+                            <div class="d-flex gap-1 align-items-center justify-content-center">
+                                <a href="{{ route('sensors.destroy', $sensor->id) }}"
+                                   data-confirm-delete="true"
+                                   class="btn btn-danger btn-sm">
+                                    <i class="fas fa-trash-alt text-white"></i>
+                                </a>
                             </div>
                         </td>
                     </tr>
@@ -51,6 +57,8 @@
                 </tbody>
             </table>
         </div>
+
     </div>
 </div>
+
 @endsection
