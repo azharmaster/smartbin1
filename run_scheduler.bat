6d201f0598c8b0e0c0b7864bf548fef91ad441e6
Nur Atirah <naatirah79@gmail.com>
2026-02-09 09:15:39 +0800

    Update run_scheduler.bat

--- run_scheduler.bat
+++ run_scheduler.bat
@@ -3,4 +3,3 @@ cd /d C:\laragon\www\smartbin1
 
 "C:\laragon\bin\php\php-8.2.30\php.exe" artisan smartbin:simulate >> "C:\laragon\www\smartbin1\storage\logs\scheduler.log" 2>&1
 
-"C:\laragon\bin\php\php-8.2.30\php.exe" artisan smartbin:undetected >> "C:\laragon\www\smartbin1\storage\logs\scheduler.log" 2>&1
