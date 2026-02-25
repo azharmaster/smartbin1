8431250f8bfa74b030627048296709830559d102
MasterCode <niazhar96@gmail.com>
2026-02-25 15:38:47 +0800

    ..

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -185,7 +185,7 @@
 
 </head>
 
-<body class="hold-transition sidebar-mini text-sm">
+<body class="hold-transition sidebar-mini text-sm @if(request()->routeIs('dashboard')) sidebar-collapse @endif">
     @include('sweetalert::alert')
 
     <div class="wrapper">
