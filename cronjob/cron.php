d5698474f8c11c388c7512695d717b21f79ab56d
MasterCode <niazhar96@gmail.com>
2026-04-13 09:17:28 +0800

    Update cron.php

--- cronjob/cron.php
+++ cronjob/cron.php
@@ -23,7 +23,7 @@ function isCollectionCapacity(float $capacity): bool
 
 function isDailyReportTime(Carbon $timestamp): bool
 {
-    return $timestamp->format('H:i') === '13:00';
+    return $timestamp->format('H:i') === '18:58';
 }
 
 function formatAssetLabel(array $asset): string
